a1b784c462a30344a71ec6b7017101bd860a93ae
Richard Wigley <richard.wigley@gmail.com>
2013-04-21 18:16:14 +0200

    Update default.rb
    trusting only a single ip address to the server. Listening on all addresses.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ default["postgresql"]["pg_hba"]  = [
   { type: "local", db: "all", user: "all",  addr: "", method: "trust"  },
   { type: "host",  db: "all", user: "all",  addr: "127.0.0.1/32", method: "trust"  },
   { type: "host",  db: "all", user: "all",  addr: "::1/128", method: "trust"  },
-  { type: "host",  db: "all", user: "all",  addr: "0.0.0.0/0", method: "md5"  }
+  { type: "host",  db: "all", user: "all",  addr: "83.67.10.5/32", method: "trust"  }
 ]
 
 default["postgresql"]["pg_hba_defaults"]                 = false
@@ -13,3 +13,5 @@ default["postgresql"]["lc_messages"]                     = "en_GB.UTF-8"
 default["postgresql"]["lc_monetary"]                     = "en_GB.UTF-8"
 default["postgresql"]["lc_numeric"]                      = "en_GB.UTF-8"
 default["postgresql"]["lc_time"]                         = "en_GB.UTF-8"
+
+default["postgresql"]["listen_addresses"] =  "*"
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "richard.wigley"
 license          "MIT"
 description      "Wrapper to postgresql cookbook maintained by phlipper/chef-postgresql"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.0.1"
 
 %w{ ubuntu}.each do |os|
   supports os
