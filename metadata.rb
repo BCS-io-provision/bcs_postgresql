766a68118f7d1bda0b65f0358ecf534b7af88d2c
Richard Wigley <Richard.Wigley@gmail.com>
2014-02-19 14:22:07 +0000

    UPDATED ubuntu version and simplified metadata

--- metadata.rb
+++ metadata.rb
@@ -6,11 +6,6 @@ description      "Wrapper to postgresql cookbook maintained by phlipper/chef-pos
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-%w{ ubuntu}.each do |os|
-  supports os
-end
+supports 'ubuntu', '>= 12.04'
 
-
-%w{ postgresql }.each do |cb|
-  depends cb
-end
+depends postgresql
