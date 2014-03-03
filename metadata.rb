32a03cea07c7c80c3c947c518b984b8c7f4d254c
RichardWigley <richard.wigley@gmail.com>
2014-03-03 21:42:40 +0000

    bump release 0.0.2 - integration tests passing

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'richard.wigley'
 license          'MIT'
 description      'wrapper postgresql for bcs ltd.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.2'
 
 supports 'ubuntu', '>= 12.04'
 
