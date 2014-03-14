ee9e6a123466f76005114371290936680ac778c2
RichardWigley <richard.wigley@gmail.com>
2014-03-14 21:21:25 +0000

    UPDATE

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,7 @@
 #
 
 ENV['LANGUAGE'] = ENV['LANG'] = ENV['LC_ALL'] = "en_GB.UTF-8"
+require 'pry' ; binding.pry
 include_recipe 'postgresql::libpq' # A debian or ubuntu requirement
 include_recipe 'postgresql::server'
 include_recipe 'postgresql::contrib'
