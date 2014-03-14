91daace09f0d7b6c4ca473d755bd8ea9409c7edf
RichardWigley <richard.wigley@gmail.com>
2014-03-14 22:00:04 +0000

    locale added back in

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,7 @@
 
+
+
+default['postgresql']['pg_hba_defaults']   = false
 default['postgresql']['pg_hba']  = [
   { type: 'local', db: 'all', user: 'all', addr: '', method: 'trust' },
   { type: 'host',  db: 'all', user: 'all', addr: '127.0.0.1/32', method: 'trust' },
@@ -6,11 +9,6 @@ default['postgresql']['pg_hba']  = [
   { type: 'host',  db: 'all', user: 'all', addr: '83.67.10.5/32', method: 'trust' }
 ]
 
-default['postgresql']['pg_hba_defaults']                 = false
-default['postgresql']['listen_addresses'] =  '*'
+default["postgresql"]["initdb_options"]    = "--locale=en_GB.UTF-8"
 
-# These settings are initialized by initdb, but they can be changed.
-default["postgresql"]["lc_messages"]                     = "en_GB.UTF-8"
-default["postgresql"]["lc_monetary"]                     = "en_GB.UTF-8"
-default["postgresql"]["lc_numeric"]                      = "en_GB.UTF-8"
-default["postgresql"]["lc_time"]                         = "en_GB.UTF-8"
+default['postgresql']['listen_addresses']  =  '*'
