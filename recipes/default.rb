714c6b0969cb72115de00d162aa7fb5e01586e22
RichardWigley <richard.wigley@gmail.com>
2014-03-14 21:17:12 +0000

    ENV to gb

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
+ENV['LANGUAGE'] = ENV['LANG'] = ENV['LC_ALL'] = "en_GB.UTF-8"
 include_recipe 'postgresql::libpq' # A debian or ubuntu requirement
 include_recipe 'postgresql::server'
 include_recipe 'postgresql::contrib'
