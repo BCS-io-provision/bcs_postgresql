5d18f9ef0f398ceb58625b3877df8e786f3701d3
RichardWigley <richard.wigley@gmail.com>
2014-03-15 11:03:22 +0000

    Added lc_xxx attributes for en_GB.UTF-8

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,5 @@
 
-
-
+# Security Settings
 default['postgresql']['pg_hba_defaults']   = false
 default['postgresql']['pg_hba']  = [
   { type: 'local', db: 'all', user: 'all', addr: '', method: 'trust' },
@@ -9,6 +8,14 @@ default['postgresql']['pg_hba']  = [
   { type: 'host',  db: 'all', user: 'all', addr: '83.67.10.5/32', method: 'trust' }
 ]
 
+# Readme says that this is enough to set locale.
+# However, I have found it needs lc_XX set.
 default["postgresql"]["initdb_options"]    = "--locale=en_GB.UTF-8"
 
+default["postgresql"]["lc_messages"]       = "en_GB.UTF-8"
+default["postgresql"]["lc_monetary"]       = "en_GB.UTF-8"
+default["postgresql"]["lc_numeric"]        = "en_GB.UTF-8"
+default["postgresql"]["lc_time"]           = "en_GB.UTF-8"
+
+# IP security setting
 default['postgresql']['listen_addresses']  =  '*'
