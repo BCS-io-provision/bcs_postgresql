09a1d7c11c024519010a1f302438cd500fcb0495
RichardWigley <richard.wigley@gmail.com>
2014-05-27 19:14:32 +0000

    bumped version of postgres to 0.14.0

--- Berksfile
+++ Berksfile
@@ -1,8 +1,8 @@
 source 'https://api.berkshelf.com'
 
 metadata
-cookbook 'bcs_locale', github: "BCS-io/bcs_locale"
-cookbook 'locale', github: "BCS-io/chef-locale"
-cookbook 'postgresql', github: "BCS-io/chef-postgresql"
+cookbook 'bcs_locale', github: 'BCS-io/bcs_locale'
+cookbook 'locale', github: 'BCS-io/chef-locale'
+cookbook 'postgresql', github: 'phlipper/chef-postgresql'
 
 
--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'richard.wigley'
 license          'MIT'
 description      'wrapper postgresql for bcs ltd.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 supports 'ubuntu', '>= 12.04'
 
 depends 'apt'
 depends 'locale', '~> 1.0.0'
 depends 'bcs_locale', '~> 1.0.0'
-depends 'postgresql', '~> 0.13.3'
+depends 'postgresql', '~> 0.14.0'
