ecf9b4c10fcd3ed93012869448d82314ecede731
RichardWigley <richard.wigley@gmail.com>
2014-06-23 10:17:54 +0000

    added contrib

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,3 +11,5 @@ ENV['LANGUAGE'] = ENV['LANG'] = ENV['LC_ALL'] = "en_GB.UTF-8"
 include_recipe 'postgresql::libpq' # A debian or ubuntu requirement
 include_recipe 'postgresql::server'
 include_recipe 'postgresql::contrib'
+
+package 'postgresql-contrib'
