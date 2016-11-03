328e9b0bc7e4cebed6b2b92801f6ca4ff7a5ac82
Richard <richard.wigley@gmail.com>
2016-11-03 10:03:45 +0000

    Update Version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'richard.wigley'
 license          'MIT'
 description      'wrapper postgresql for bcs ltd.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.0'
+version          '1.1.1'
 
 supports 'ubuntu', '>= 12.04'
 
