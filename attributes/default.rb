b1d87504c7c3cb451eff02141c3466632df91d6a
Richard <richard.wigley@gmail.com>
2016-11-03 09:59:37 +0000

    Rubocopped

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,6 @@
-
 # Security Settings
-default['postgresql']['pg_hba_defaults']   = false
-default['postgresql']['pg_hba']  = [
+default['postgresql']['pg_hba_defaults'] = false
+default['postgresql']['pg_hba'] = [
   { type: 'local', db: 'all', user: 'all', addr: '', method: 'trust' },
   { type: 'host',  db: 'all', user: 'all', addr: '127.0.0.1/32', method: 'trust' },
   { type: 'host',  db: 'all', user: 'all', addr: '::1/128', method: 'trust' },
@@ -10,12 +9,12 @@ default['postgresql']['pg_hba']  = [
 
 # Readme says that this is enough to set locale.
 # However, I have found it needs lc_XX set.
-default["postgresql"]["initdb_options"]    = "--locale=en_GB.UTF-8"
+default['postgresql']['initdb_options']    = '--locale=en_GB.UTF-8'
 
-default["postgresql"]["lc_messages"]       = "en_GB.UTF-8"
-default["postgresql"]["lc_monetary"]       = "en_GB.UTF-8"
-default["postgresql"]["lc_numeric"]        = "en_GB.UTF-8"
-default["postgresql"]["lc_time"]           = "en_GB.UTF-8"
+default['postgresql']['lc_messages']       = 'en_GB.UTF-8'
+default['postgresql']['lc_monetary']       = 'en_GB.UTF-8'
+default['postgresql']['lc_numeric']        = 'en_GB.UTF-8'
+default['postgresql']['lc_time']           = 'en_GB.UTF-8'
 
 # IP security setting
-default['postgresql']['listen_addresses']  =  '*'
+default['postgresql']['listen_addresses'] = '*'
