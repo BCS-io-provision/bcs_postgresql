5c21b5bd8bc958b3cabb1689a34794cb87586ae2
Richard <richard.wigley@gmail.com>
2016-12-22 22:23:50 +0000

    Chef 12 metadata required

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description      'wrapper postgresql for bcs ltd.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.4'
 
+source_url       'https://github.com/BCS-io-provision/bcs_postgresql' if respond_to?(:source_url)
+issues_url       'https://github.com/BCS-io-provision/bcs_postgresql/issues' if respond_to?(:issues_url)
+
 supports 'ubuntu', '>= 14.04'
 
 depends 'apt'
