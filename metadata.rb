9ed89e12ec6962c05ea3e7a131cb441ae3339084
Richard <richard.wigley@gmail.com>
2017-04-24 12:39:51 +0100

    Version Bump -> 2.2.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'richard.wigley'
 license          'MIT'
 description      'wrapper postgresql for bcs ltd.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.2.0'
+version          '2.2.1'
 
 source_url       'https://github.com/BCS-io-provision/bcs_postgresql' if respond_to?(:source_url)
 issues_url       'https://github.com/BCS-io-provision/bcs_postgresql/issues' if respond_to?(:issues_url)
