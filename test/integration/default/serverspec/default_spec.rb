87cb9f0003244908365cc68a975ded151152f58b
Richard <richard.wigley@gmail.com>
2018-05-03 09:32:57 +0100

    Improved testing to cover postgres running
      - Ubuntu 16.04's big change is systemd
        - adding test to check it is running under systemd

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -7,4 +7,15 @@ describe 'bcs_postgresql::default' do
     app_version = os[:release] == '16.04' ? 'postgresql-9.5' : 'postgresql-9.3'
     expect(package(app_version)).to be_installed
   end
+
+  describe service 'postgresql' do
+    if os[:release] == '14.04'
+      # be_enabled was false - but moving away from 14 so not
+      # investigating any further
+      it { should be_running }
+    elsif os[:release] == '16.04'
+      it { should be_enabled }
+      it { should be_running.under('systemd') }
+    end
+  end
 end
